968dd188969466728f0215060e299b649ae5f0b2
Jeremiah Black <jeremiahblackol@gmail.com>
2019-12-20 11:01:43 -0700

    got clicked status to stick on buttons

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var parent = document.querySelector('.header-styles');
 var imagePlacer = document.querySelector('.appear-img');
+var buttonStuff = document.querySelectorAll('.button-styles')
 
 parent.addEventListener('click', somethingHappens);
 
 function somethingHappens(event) {
+  event.target.classList.add('clicked-status');
+  buttonStuff.classList.remove('button-styles');
   console.log('hey');
+
 }
