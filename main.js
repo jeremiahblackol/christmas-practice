37d201f737973bb4e765ec25085fb09c343a3056
Jeremiah Black <jeremiahblackol@gmail.com>
2019-12-20 10:53:13 -0700

    added event listener to parent container

---
+++ main.js
@@ -0,0 +1,8 @@
+var parent = document.querySelector('.header-styles');
+var imagePlacer = document.querySelector('.appear-img');
+
+parent.addEventListener('click', somethingHappens);
+
+function somethingHappens(event) {
+  console.log('hey');
+}
